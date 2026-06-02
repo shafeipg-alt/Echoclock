b090acd8021d91ebc62d3f24f7bff652226ab217
Mika <1179720183@qq.com>
2026-06-02 16:29:32 +0800

    Initial Commit

---
+++ EchoClock/EchoClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoClockApp.swift
+//  EchoClock
+//
+//  Created by 沙飞 on 2026/6/2.
+//
+
+import SwiftUI
+
+@main
+struct EchoClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
